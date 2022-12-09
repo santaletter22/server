d0525f01d7ce6a4a07182d6bddc1c1d82eb630b2
hwan <hshlego@naver.com>
2022-12-09 20:04:33 +0900

    initial commit

---
+++ src/test/java/com/project/santaletter/SantaletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.santaletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
